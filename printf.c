2fbc98aa7d075f5f7433d173c27808bb647e4753
chukwudozie <emkychuks062@gmail.com>
2022-08-09 09:52:11 -0700

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Custom function to print to STDOUT
+ * @format: input character string
+ * Return: Number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
